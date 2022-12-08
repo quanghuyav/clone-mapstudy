a1fec44d2bfe2fd21c872b4668b4e24010a6fb09
quanghuyav <huyav1999@gmail.com>
2022-12-09 00:58:31 +0700

    add login form

--- src/layouts/DefaultLayout/FormLogin/FormLogin.js
+++ src/layouts/DefaultLayout/FormLogin/FormLogin.js
@@ -7,51 +7,26 @@ const cx = classNames.bind(styles);
 
 function FormLogin() {
     return (
-        <div className={cx('overlay')}>
+        <div className={cx('wrapper')}>
+            <div className={cx('header')}>Đăng nhập</div>
             <form>
-                <div className={cx('con')}>
-                    <header className={cx('head-form')}>
-                        <h2>Log In</h2>
-                        <p>login here using your username and password</p>
-                    </header>
-                    <br />
-                    <div className={cx('field-set')}>
-                        <span className={cx('input-item')}>
-                            <FontAwesomeIcon icon={faCircleUser} />
-                        </span>
-                        <input
-                            className={cx('form-input')}
-                            id="txt-input"
-                            type="text"
-                            placeholder="UserName"
-                            required
-                        />
-                        <br />
-                        <span className={cx('input-item')}>
-                            <FontAwesomeIcon icon={faKey} />
-                        </span>
-                        <input
-                            className={cx('form-input')}
-                            type="password"
-                            placeholder="Password"
-                            id="pwd"
-                            name="password"
-                            required
-                        />
-                        <span>
-                            <i className={cx('fa fa-eye')} aria-hidden="true" type="button" id="eye" />
-                        </span>
-                        <br />
-                        <button className={cx('log-in')}> Log In </button>
-                    </div>
-                    <div className={cx('other')}>
-                        <button className={cx('btn submits frgt-pass')}>Forgot Password</button>
-                        <button className={cx('btn submits sign-up')}>
-                            Sign Up
-                            <i className={cx('fa fa-user-plus')} aria-hidden="true" />
-                        </button>
-                    </div>
+                <label htmlFor={cx('username')}>Tên đăng nhập:</label>
+                <input type="text" id={cx('username')} name={cx('username')} placeholder="Enter your username"></input>
+                <label htmlFor={cx('password')}>Mật khẩu:</label>
+                <input
+                    type="password"
+                    id={cx('password')}
+                    name={cx('password')}
+                    placeholder="Enter your password"
+                ></input>
+                <div className={cx('signin')}>
+                    <span>Bạn chưa có tài khoản? </span>
+                    <a href="/signin">Đăng kí</a>
                 </div>
+                <input type="submit" value="Đăng nhập" className={cx('login-btn')}></input>
+                <a className={cx('forget-password')} href="/signin">
+                    Quên mật khẩu?
+                </a>
             </form>
         </div>
     );
